9326480abcfbb8353a2e4b4d20b34004e39bff5b
leonkees <112914301+leonkees@users.noreply.github.com>
2026-05-05 14:57:30 +0200

    Update post migracion para usar MCP claude
    muchos cambios sin backupear, controls, spells, mapa level

---
+++ Source/ActionBuilder/DummyCPP.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DummyCPP.h"
+
+// Sets default values
+ADummyCPP::ADummyCPP()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADummyCPP::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADummyCPP::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
